3e5a0acaeff14c876f7c24a12ffce82181a1740c
udithaGurusinghe2004 <chamithudithaedu@gmail.com>
2025-11-28 20:47:20 +0530

    edited shirt

--- src/model/Shirt.java
+++ src/model/Shirt.java
@@ -8,13 +8,31 @@ public class Shirt extends Cloth{
         super(id,name,quantity,price,color,size);
         this.sleeveType = sleeveType;
     }
-    @Override
-    public String getCategory() {
-        return "Shrit";
-    }
 
     @Override
     public String toString() {
         return super.toString()+"\n"+sleeveType;
     }
-}
+
+    @Override
+    public String toCsv() {
+        return String.join(",",
+                nullSafe(getId()),
+                nullSafe(getName()),
+                String.valueOf(getQuantity()),
+                String.valueOf(getPrice()),
+                nullSafe(getColor()),
+                nullSafe(getSize()),
+                nullSafe(sleeveType),
+                "Shirt"
+        // category (also fix getCategory to return "Shirt")
+                );
+    }
+    private static String nullSafe(String s) {
+        return s == null ? "" : s;
+    }
+    @Overridepublic String getCategory() {
+        return "Shirt";
+    } // fix typo from "Shrit"
+
+    }}
